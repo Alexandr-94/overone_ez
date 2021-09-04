83d1663dca6336cb16bed45a770bf9c8cd881131
Alexandr <santer4249@mail.ru>
2021-09-04 19:55:45 +0300

    i am making task 1

--- prover_1.py
+++ prover_1.py
@@ -1,9 +1,17 @@
-num_card = input('введите номер карты')
+num_card = ('1234 6321 4563 9654' ).split()
+def credit():
+    num_len = 0
+    new_num = ''
+    for i in num_card:
+        num_len += 1
+        if num_len != 4:
+            new_num += '**** '
+        else:
+            new_num += i
+    print(new_num)
+credit()
 
-def credit(num):
-    return num
 
-print(credit(num_card))
 
 def palindrom(s):
 
